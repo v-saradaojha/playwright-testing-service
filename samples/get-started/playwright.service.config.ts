07900d32a0026dea72ced2b40558bb7058c0f5c8
abranj19 <134475350+abranj19@users.noreply.github.com>
2024-04-11 02:56:39 +0530

    Update playwright.service.config.ts

--- samples/get-started/playwright.service.config.ts
+++ samples/get-started/playwright.service.config.ts
@@ -21,7 +21,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Name the test run if it's not named yet.
-process.env.PLAYWRIGHT_SERVICE_RUN_ID = process.env.PLAYWRIGHT_SERVICE_RUN_ID || new Date().toISOString();
+//process.env.PLAYWRIGHT_SERVICE_RUN_ID = process.env.PLAYWRIGHT_SERVICE_RUN_ID || new Date().toISOString();
 
 // Can be 'linux' or 'windows'.
 const os = process.env.PLAYWRIGHT_SERVICE_OS || 'linux';
