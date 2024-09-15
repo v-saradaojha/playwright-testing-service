96130068d0b0a5833513a1bf49825ba63c44e3bf
Sarada Ojha <v-saradaojha@microsoft.com>
2024-09-15 13:52:02 +0530

    test changes

--- samples/get-started/tests/example.spec.ts
+++ samples/get-started/tests/example.spec.ts
@@ -1,24 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 // This sample simulates a larger test suite
-var tags=['@smoke', '@sanity', '@issue'];
+const tags = ['@smoke', '@sanity', '@issue'];
 //eastasia test//
-const TEST_ITERATIONS = parseInt(process.env.TEST_ITERATIONS || "6000");
-for (var i = 0; i < TEST_ITERATIONS; i++) {
-  var tagid= i % 3;
-  test('has title ' + i+ tags[tagid], async ({ page }) => {
-    const skip= Math.random();
-    if (skip > 0.9)
-     {
-       test.skip();
-     }
+const TEST_ITERATIONS = parseInt(process.env.TEST_ITERATIONS || "10");
+for (let i = 0; i < TEST_ITERATIONS; i++) {
+  let tagid = i % 3;
+  test('has title ' + i + tags[tagid], async ({ page }) => {
+    const skip = Math.random();
+    if (skip > 0.9) {
+      test.skip();
+    }
     await page.goto('https://playwright.dev/');
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Playwright/);
   });
 
-  test('get started link ' + i+ tags[tagid], async ({ page }) => {
+  test('get started link ' + i + tags[tagid], async ({ page }) => {
     await page.goto('https://playwright.dev/');
 
     // Click the get started link.
@@ -26,7 +25,7 @@ for (var i = 0; i < TEST_ITERATIONS; i++) {
 
     // Expects the URL to contain intro.
     await expect(page).toHaveURL(/.*intro/);
-    
+
     const randomResult = Math.random();
     expect(randomResult).toBeGreaterThan(0.2);
   });
